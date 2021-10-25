474b0ceff867d8b1c4c1f82784a50ffdaf7313cf
fbcampos <70235848+fbcampos@users.noreply.github.com>
2021-10-25 15:11:30 -0300

    Delete Swift directory

--- Swift/SelfService Ticket/scriptHeader.js
+++
@@ -1,9 +0,0 @@
-var onloadCallback = function() {
-        grecaptcha.render('html_element', {
-          'sitekey' : '6Lcbm9UbAAAAAP8HltC7JkKipmXEcUY2ZaSvTLiD',
-          'callback': verifyCallback
-        });
-      };
-var verifyCallback = function(response) {
-        validaPoliticaPrivacRecaptcha();
-      };
